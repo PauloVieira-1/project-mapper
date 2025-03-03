df34c591a2abe642bd634122e4af1813ece91d76
Paulo Vieira <paulo1vieira.2004@gmail.com>
2025-03-03 15:50:36 +0100

    update webview icon

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,49 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
+import { time } from 'console';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vs-window" is now active!');
+	const iconPath = vscode.Uri.file(
+		path.join(context.extensionPath, 'icons', 'icon.png')
+	  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	const panel = vscode.commands.registerCommand('projectmapper.launch', () => {
 
-	const disposable = vscode.commands.registerCommand('projectmapper.launch', () => {
+		setTimeout(() => {
+			vscode.window.showInformationMessage('Opening Project-Mapper...');
+		}, 1000);
 
-		vscode.window.showInformationMessage('Hello World from VS-Window!');
-		// vscode.window.showOpenDialog();
-		vscode.window.createWebviewPanel('.pm', 'Project-Mapper', vscode.ViewColumn.Two);
-		
+		vscode.window.createWebviewPanel('projectMapper', 'Project-Mapper', vscode.ViewColumn.Two, {
+			enableScripts: true,
+			retainContextWhenHidden: true,
+			localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'icons'))]
+		  }).webview.html = getWebViewContent();
 	});
 
-	context.subscriptions.push(disposable);
+	const getWebViewContent = () => {
+		return `<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Project-Mapper</title>
+			<link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" />
+			<script src="https://cdn.tailwindcss.com"></script>
+		</head>
+		<body>
+		<div class="container mx-auto my-10">
+		<h1 class="text-3xl font-bold my-5">Project-Mapper</h1>
+		<a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href="https://github.com/PauloVieira-1/project-mapper" target="_blank" rel="noopener noreferrer">
+			  Go to Github
+		</a>			
+		</div>
+		</body>
+		</html>`;
+	  };
+
+
+	context.subscriptions.push(panel);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
